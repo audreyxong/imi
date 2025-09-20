be05d547f62d8dcff524229c5d1c17263b1a35eb
audreyxong <audreyxong@gmail.com>
2025-09-21 04:19:55 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
   async headers() {
     return [
       {
         source: "/:path*",
         headers: [
           { key: "Content-Security-Policy", value: "frame-ancestors 'self' https://imicorp.com.sg;" },
-          { key: "X-Frame-Options", value: "ALLOW-FROM https://imicorp.com.sg" }
-        ]
-      }
+          { key: "X-Frame-Options", value: "ALLOW-FROM https://imicorp.com.sg" },
+        ],
+      },
     ];
-  }
+  },
 };
-
 export default nextConfig;
+
